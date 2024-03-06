f469dc0e69f3d7a6dba978f3a8719086c7244bd7
GhostDrew11 <armelhell@gmail.com>
2024-03-05 23:12:16 -0500

    Graceful Shutdown

--- cmd/vigor-api/main.go
+++ cmd/vigor-api/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/GhostDrew11/vigor-api/internal/api"
 	"github.com/GhostDrew11/vigor-api/internal/config"
@@ -14,25 +19,47 @@ func main() {
 	//Load configuration
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		log.Printf("Error loading config: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Error loading config: %v\n", err)
 	}
 
 	//Connect to the database
 	if err := db.ConnectDB(cfg); err != nil {
-		log.Printf("Failed to connect to MongoDB: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Failed to connect to MongoDB: %v\n", err)
 	}
 	defer db.DisconnectDB()
 
 	// Set up your Gin router
 	router := gin.Default()
 
-	// Set up you routes
+	// Set up your routes
 	api.SetupRoutes(router)
 
-	// Start you API server
-	if err := router.Run(":8080"); err != nil {
-		log.Fatal("Server failed to start:", err)
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: router,
 	}
+
+	// Start API server in a go routine
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v\n", err)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	// catch SIGINT and SIGTERM signals
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// Doesn't block if no connections, but will wait for the duration of the context deadline
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
+
+	log.Println("Server exiting")
 }
