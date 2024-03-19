fbe7a54e1a14f62f91cbb399c745f5e99f02d0c4
GhostDrew11 <armelhell@gmail.com>
2024-03-18 20:58:36 -0400

    Failure Cases

--- tests/unit_tests/middlewares_test/auth_test.go
+++ tests/unit_tests/middlewares_test/auth_test.go
@@ -1,6 +1,7 @@
 package middlewares_test
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -22,6 +23,7 @@ type MockJWTService struct {
 
 func (m *MockJWTService) GenerateAccessToken(userId primitive.ObjectID, email string) (string, error) {
 	args := m.Called(userId, email)
+	// tokenStr, _ := args.String(0)
 	return args.String(0), args.Error(1)
 }
 
@@ -36,7 +38,7 @@ func (m *MockJWTService) VerifyToken(tokenString string) (*utils.Claims, error) 
 	return claims, args.Error(1)
 }
 
-func TestAuthenticateMiddleware(t *testing.T) {
+func TestAuthenticateMiddlewareSuccess(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	router := gin.New()
 
@@ -67,6 +69,48 @@ func TestAuthenticateMiddleware(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
+	mockJWTService.AssertExpectations(t)
+}
+
+func TestAuthenticateMiddlewareWithEmptyToken(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	mockJWTService := new(MockJWTService)
+
+	router.Use(middlewares.Authenticate(mockJWTService))
+	router.GET("/test", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"message": "Should not get here"})
+    })
+
+	w := httptest.NewRecorder()
+    req, _ := http.NewRequest("GET", "/test", nil)
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+}
+
+func TestAuthenticateMiddlewareFailureTokenVerification(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	mockJWTService := new(MockJWTService)
+	invalidTokenString := "invalidtokenstring"
+
+	mockJWTService.On("VerifyToken", invalidTokenString).Return(nil, utils.ErrInvalidToken)
+
+	router.Use(middlewares.Authenticate(mockJWTService))
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Should not reach"})
+	})
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/test", nil)
+	req.Header.Set("Authorization", invalidTokenString)
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
 	mockJWTService.AssertExpectations(t)
 }
 
@@ -106,5 +150,73 @@ func TestRefreshHandlerMiddlewareSuccess(t *testing.T) {
 	assert.Contains(t, w.Body.String(), newAccessToken)
 	assert.Contains(t, w.Body.String(), newRefreshToken)
 	mockJWTService.AssertExpectations(t)
+}
+
+func TestRefreshHandlerMiddlewareFailureEmptyToken(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	mockJWTService := new(MockJWTService)
+
+	router.POST("/refresh", middlewares.RefreshHandler(mockJWTService))
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/refresh", nil)
+
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusUnauthorized, w.Code) 
+}
+
+func TestRefreshHandlerMiddlewareFailureTokenVerification(t *testing.T) {
+    gin.SetMode(gin.TestMode)
+    router := gin.New()
+
+    mockJWTService := new(MockJWTService)
+    invalidRefreshToken := "invalidRefreshToken"
+
+    mockJWTService.On("VerifyToken", invalidRefreshToken).Return(nil, utils.ErrInvalidToken)
+
+    router.POST("/refresh", middlewares.RefreshHandler(mockJWTService))
+
+    w := httptest.NewRecorder()
+    req, _ := http.NewRequest("POST", "/refresh", nil)
+    req.Header.Set("Refresh-Token", invalidRefreshToken)
+
+    router.ServeHTTP(w, req)
+
+    assert.Equal(t, http.StatusUnauthorized, w.Code) 
+    mockJWTService.AssertExpectations(t)
+}
+
+func TestRefreshHandlerMiddlewareFailureTokenGeneration(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	mockJWTService := new(MockJWTService)
+	userId := primitive.NewObjectID()
+	email := "test@example.com"
+
+	refreshToken := "refreshTokenDummy"
+	claims := &utils.Claims{
+		UserId: userId,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+		},
+	}
+
+	mockJWTService.On("VerifyToken", refreshToken).Return(claims, nil)
+	mockJWTService.On("GenerateAccessToken", userId, email).Return("", errors.New("Unable to generate access token"))
+
+	router.POST("/refresh", middlewares.RefreshHandler(mockJWTService))
+
+	w := httptest.NewRecorder()
+    req, _ := http.NewRequest("POST", "/refresh", nil)
+    req.Header.Set("Refresh-Token", refreshToken)
+
+    router.ServeHTTP(w, req)
+
+    assert.Equal(t, http.StatusInternalServerError, w.Code) 
+    mockJWTService.AssertExpectations(t)
 }
 
