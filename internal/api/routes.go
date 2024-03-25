1dcbc00029d1d3d19dfaf647b27a088487832f44
GhostDrew11 <armelhell@gmail.com>
2024-03-25 09:49:37 -0400

    Removed unnecessary comments

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,108 +2,123 @@ package api
 
 import (
 	// 	"github.com/GhostDrew11/vigor-api/internal/middlewares"
+	"github.com/GhostDrew11/vigor-api/internal/middlewares"
 	"github.com/GhostDrew11/vigor-api/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine, ts utils.TokenService) {
-// 	// API root
-// 	apiRoot := router.Group("/api/v1")
 
-// 	// Auth routes
-// 	authRoutes := apiRoot.Group("/auth")
-// 	authRoutes.POST("/refresh", middlewares.RefreshHandler(ts))
+	// API root
+	apiRoot := router.Group("/api/v1")
 
-// 	// Admin routes
-// 	// Workout Plan routes
-// 	adminRoutes := apiRoot.Group("/admin")
-// 	adminRoutes.POST("/wokout-plans", createWorkoutPlan)
-// 	adminRoutes.PUT("/workout-plans/:id", updateWorkoutPlan)
-// 	adminRoutes.DELETE("/workout-plans/:id", deleteWorkoutPlan)
-// 	adminRoutes.GET("/workout-plans", getWorkoutPlans)
-// 	// Meal Plan routes
-// 	adminRoutes.POST("/meal-plans", createMealPlan)
-// 	adminRoutes.PUT("/meal-plans/:id", updateMealPlan)
-// 	adminRoutes.DELETE("/meal-plans/:id", deleteMealPlan)
-// 	adminRoutes.GET("/meal-plans", getMealPlans)
-// 	// Exercise routes
-// 	adminRoutes.POST("/exercises", createExercise)
-// 	adminRoutes.PUT("/exercises/:id", updateExercise)
-// 	adminRoutes.DELETE("/exercises/:id", deleteExercise)
-// 	adminRoutes.GET("/exercises", getExercises)
-// 	// Meal routes
-// 	adminRoutes.POST("/meals", createMeal)
-// 	adminRoutes.PUT("/meals/:id", updateMeal)
-// 	adminRoutes.DELETE("/meals/:id", deleteMeal)
-// 	adminRoutes.GET("/meals", getMeals)
-// 	// Add more admin routes as needed (e.g., get user workout plans, get user meal plans, etc.)
+	// Auth routes
+	authRoutes := apiRoot.Group("/auth")
+	authRoutes.POST("/login", login)
+	authRoutes.POST("/register", register)
+	authRoutes.POST("/refresh", middlewares.RefreshHandler(ts))
 
-// 	// User routes
-// 	userRoutes := apiRoot.Group("/users")
-// 	userRoutes.POST("/register", registerUser)
-// 	userRoutes.POST("/login", loginUser)
+	// Admin routes
+	adminRoutes := apiRoot.Group("/admin")
+	adminRoutes.Use(middlewares.RequireRole(ts, "admin"))
+	// CRUD Exercises
+	adminRoutes.POST("/exercises", createExercise)
+	adminRoutes.PUT("/exercises/:id", updateExercise)
+	adminRoutes.DELETE("/exercises/:id", deleteExercise)
+	adminRoutes.GET("/exercises", getExercises)
+	// CRUD Workout Plans
+	adminRoutes.POST("/workout-plans", createWorkoutPlan)
+	adminRoutes.PUT("/workout-plans/:id", updateWorkoutPlan)
+	adminRoutes.DELETE("/workout-plans/:id", deleteWorkoutPlan)
+	adminRoutes.GET("/workout-plans", getWorkoutPlans)
+	// CRUD Meals
+	adminRoutes.POST("/meals", createMeal)
+	adminRoutes.PUT("/meals/:id", updateMeal)
+	adminRoutes.DELETE("/meals/:id", deleteMeal)
+	adminRoutes.GET("/meals", getMeals)
+	// CRUD Meal Plans
+	adminRoutes.POST("/meal-plans", createMealPlan)
+	adminRoutes.PUT("/meal-plans/:id", updateMealPlan)
+	adminRoutes.DELETE("/meal-plans/:id", deleteMealPlan)
+	adminRoutes.GET("/meal-plans", getMealPlans)
+	// CRUD Admins
+	adminRoutes.POST("/admins", createAdmin)
+	adminRoutes.PUT("/admins/:id", updateAdmin)
+	adminRoutes.DELETE("/admins/:id", deleteAdmin)
+	adminRoutes.GET("/admins", getAdmins)
+	// CRUD Users
+	adminRoutes.GET("/users", getUsers)
+	// other admin routes as needed(eg list users with active subscriptions, list users with pending subscriptions, list of sales, other analytics etc.)
 
-// 	// Authenticated user routes
-// 	authenticatedUserRoutes := userRoutes.Group("/")
-// 	authenticatedUserRoutes.Use(middlewares.Authenticate(ts))
-// 	authenticatedUserRoutes.GET("/profile", getUserProfile)
-// 	authenticatedUserRoutes.PUT("/profile", updateUserProfile)
-// 	authenticatedUserRoutes.GET("/preferences", getUserPreferences)
-// 	authenticatedUserRoutes.PUT("/preferences", updateUserPreferences)
-// 	authenticatedUserRoutes.GET("/subscription", getUserSubscription)
-// 	authenticatedUserRoutes.PUT("/subscription", updateUserSubscription)
-// 	authenticatedUserRoutes.DELETE("/subscription", cancelUserSubscription)
-// 	authenticatedUserRoutes.DELETE("/account", deleteUserAccount)
+	// User routes
+	userRoutes := apiRoot.Group("/users")
+	userRoutes.Use(middlewares.RequireRole(ts, "user"))
+	// CRUD User data
+	userRoutes.GET("/profile", getUserProfile)
+	userRoutes.PUT("/profile", updateUserProfile)
+	userRoutes.GET("/preferences", getUserPreferences)
+	userRoutes.PUT("/preferences", updateUserPreferences)
+	userRoutes.GET("/subscription", getUserSubscription)
+	userRoutes.PUT("/subscription", updateUserSubscription)
+	userRoutes.DELETE("/subscription", cancelUserSubscription)
+	userRoutes.DELETE("/account", deleteUserAccount)
+	// other user routes as needed(eg list user workout plans, list user meal plans, list user progress, other analytics etc.)
 
-// 	// Search for a user via it's username to start a new conversation
-// 	authenticatedUserRoutes.GET("/search", searchUser)
+	// Progress tracking routes
+	// Workout Plan
+	userRoutes.POST("/workout-plans/:workoutPlanId/progress", createWorkoutPlanProgress)
+	userRoutes.GET("/workout-plans/:workoutPlanId/progress", getWorkoutPlanProgress)
+	userRoutes.PUT("/workout-plans/:workoutPlanId/progress", updateWorkoutPlanProgress)
+	// Meal Plan
+	userRoutes.POST("/meal-plans/:mealPlanId/progress", createMealPlanProgress)
+	userRoutes.GET("/meal-plans/:mealPlanId/progress", getMealPlanProgress)
+	userRoutes.PUT("/meal-plans/:mealPlanId/progress", updateMealPlanProgress)
+	// Daily nutritional logs
+	userRoutes.POST("/nutritional-logs", createNutritionalLog)
+	// Maybe use them to build some analytics and graphs you can show to the user
+	userRoutes.GET("/nutritional-logs", getNutritionalLogs)
+	userRoutes.PUT("/nutritional-logs/:id", updateNutritionalLog)
 
-// 	// Progress tracking routes
-// 	// Workout Plan
-// 	authenticatedUserRoutes.POST("/workout-plans/:workoutPlanId/progress", createWorkoutPlanProgress)
-// 	authenticatedUserRoutes.GET("/workout-plans/:workoutPlanId/progress", getWorkoutPlanProgress)
-// 	authenticatedUserRoutes.PUT("/workout-plans/:workoutPlanId/progress", updateWorkoutPlanProgress)
-// 	// Meal Plan
-// 	authenticatedUserRoutes.POST("/meal-plans/:mealPlanId/progress", createMealPlanProgress)
-// 	authenticatedUserRoutes.GET("/meal-plans/:mealPlanId/progress", getMealPlanProgress)
-// 	authenticatedUserRoutes.PUT("/meal-plans/:mealPlanId/progress", updateMealPlanProgress)
-
-// 	// Daily nutritional logs
-// 	authenticatedUserRoutes.POST("/nutritional-logs", createNutritionalLog)
-// 	// Maybe use them to build some analytics and graphs you can show to the user
-// 	authenticatedUserRoutes.GET("/nutritional-logs", getNutritionalLogs)
-
-// 	// Authenticated user direct message conversations
-// 	authenticatedUserRoutes.POST("/conversations", createConversation)
-// 	// Delete a conversation
-// 	authenticatedUserRoutes.DELETE("/conversations/:conversationId", deleteConversation)
-// 	// Get all conversations for a user
-// 	authenticatedUserRoutes.GET("/conversations", getConversations)
-// 	// Send Message in a conversation
-// 	authenticatedUserRoutes.POST("/conversations/:conversationId/messages", sendMessage)
-// 	// Read a message in a conversation
-// 	authenticatedUserRoutes.GET("/conversations/:conversationId/messages/:messageId", readMessage)
-// 	// Update message content in a conversation
-// 	authenticatedUserRoutes.PUT("/conversations/:conversationId/messages/:messageId", updateMessage)
-// 	// Delete a message in a conversation
-// 	authenticatedUserRoutes.DELETE("/conversations/:conversationId/messages/:messageId", deleteMessage)
-// 	// Get all messages in a conversation
-// 	authenticatedUserRoutes.GET("/conversations/:conversationId/messages", getMessages)
-
-// 	// Two group routes because group might have other functionalities that just chatting within the group
-// 	// Authenticated user group conversations
-// 	authenticatedUserRoutes.POST("/groups", createGroup)
-// 	// Delete a group the user created
-// 	authenticatedUserRoutes.DELETE("/groups/:groupId", deleteGroup)
-// 	// Leave a group
-// 	authenticatedUserRoutes.DELETE("/groups/:groupId/leave", leaveGroup)
-// 	// Link Group to Conversation
-// 	authenticatedUserRoutes.POST("/groups/:groupId/conversations", createGroupChat)
-// 	// Send a message in a group conversation
-// 	authenticatedUserRoutes.POST("/groups/:groupId/conversations/:conversationId/messages", sendGroupMessage)
-// 	// update group message content
-// 	authenticatedUserRoutes.PUT("/groups/:groupId/conversations/:conversationId/messages/:messageId", updateGroupMessage)
-// 	// Remove a user from a group
-// 	authenticatedUserRoutes.DELETE("/groups/:groupId/members/:userId", removeGroupMember)
-// 	// Other group functionalities as needed (e.g, add member, join a group, having a group live workout party etc.)
+	// Interactions with other users routes(search, chat, etc.)
+	userRoutes.GET("/search", searchUser)
+	userRoutes.POST("/conversations", createConversation)
+	userRoutes.GET("/conversations", getConversations)
+	// Not sure if we need to update a conversation
+	userRoutes.PUT("/conversations/:conversationId", updateConversation)
+	userRoutes.DELETE("/conversations/:conversationId", deleteConversation)
+	// Send Message in a conversation
+	userRoutes.POST("/conversations/:conversationId/messages", sendMessage)
+	// Read a message in a conversation
+	userRoutes.GET("/conversations/:conversationId/messages/:messageId", readMessage)
+	// Update message content in a conversation
+	userRoutes.PUT("/conversations/:conversationId/messages/:messageId", updateMessage)
+	// Delete a message in a conversation just for the sender
+	userRoutes.DELETE("/conversations/:conversationId/messages/:messageId", deleteMessage)
+	// Get all messages in a conversation
+	userRoutes.GET("/conversations/:conversationId/messages", getMessages)
+	// Interactions within a group
+	userRoutes.POST("/groups", createGroup)
+	// Update a group
+	userRoutes.PUT("/groups/:groupId", updateGroup)
+	// Delete a group the user created
+	userRoutes.DELETE("/groups/:groupId", deleteGroup)
+	// Leave a group
+	userRoutes.DELETE("/groups/:groupId/leave", leaveGroup)
+	// Create a group conversation
+	userRoutes.POST("/groups/:groupId/conversations", createGroupChat)
+	// Send a message in a group conversation
+	userRoutes.POST("/groups/:groupId/conversations/:conversationId/messages", sendGroupMessage)
+	// Read a message in a group conversation
+	userRoutes.GET("/groups/:groupId/conversations/:conversationId/messages/:messageId", readGroupMessage)
+	// Update message content in a group conversation
+	userRoutes.PUT("/groups/:groupId/conversations/:conversationId/messages/:messageId", updateGroupMessage)
+	// Delete a message in a group conversation just for the sender
+	userRoutes.DELETE("/groups/:groupId/conversations/:conversationId/messages/:messageId", deleteGroupMessage)
+	// Get all messages in a group conversation
+	userRoutes.GET("/groups/:groupId/conversations/:conversationId/messages", getGroupMessages)
+	// Add a member to a group
+	userRoutes.POST("/groups/:groupId/members", addGroupMember)
+	// Remove a user from a group
+	userRoutes.DELETE("/groups/:groupId/members/:userId", removeGroupMember)
+	// Other group functionalities as needed (e.g, add member, join a group, having a group live workout party etc.)
 }
